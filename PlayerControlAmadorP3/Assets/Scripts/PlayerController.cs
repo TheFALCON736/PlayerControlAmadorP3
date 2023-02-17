9592969f38e8965ceda4d795a689683c0d5403fc
TheFALCON736 <angel7amador06@gmail.com>
2023-02-17 11:43:08 -0800

    hi
    hi

--- PlayerControlAmadorP3/Assets/Scripts/PlayerController.cs
+++ PlayerControlAmadorP3/Assets/Scripts/PlayerController.cs
@@ -5,27 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private Variables
-    private float speed = 5.0f;
-    private float turnSpeed = 25.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 70.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // This is where we get player input
+        // Axis setup
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
-        // We move the vehicle foward
+        // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // We turn the vehicle
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        // Rotate the vehicle left and right
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
+
